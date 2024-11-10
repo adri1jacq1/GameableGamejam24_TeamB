77910774b02e2a9adee4de708c1390b7b8e6ffe5
Salar <salar.rezayani@gmail.com>
2024-11-10 15:31:58 -0500

    spawning time

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject charachterBar360;
+    public FoodSpawnHandler foodSpawnHandler;
     public static GameManager Instance { get; private set; }
-    public float TotalGameTime = 60f;
-    private float gameTime;
+    public float healthStartTime = 20f;
+    public float lvlConstTime = 30;
+    public float lvlTime;
+    private float healthTime;
+    float gameTime;
     public TMPro.TextMeshProUGUI timeText;
+    public int eatingFoodHealthAmount = 5;
+    public int rottenFoodHealthAmount = 2;
+    int lvl;
     private void Awake()
     {
         if (Instance == null)
@@ -21,29 +28,51 @@ public class GameManager : MonoBehaviour
     }
     private void Start()
     {
-        gameTime = 0;
+        if (foodSpawnHandler ==null) foodSpawnHandler = FindFirstObjectByType<FoodSpawnHandler>();
+        healthTime = healthStartTime;
+        lvl = 0;
     } 
 
     private void Update()
     {
+        healthTime -= Time.deltaTime;
         gameTime += Time.deltaTime;
-        timeText.text = gameTime.ToString("F2");
-        charachterBar360.transform.rotation = Quaternion.Euler(0, 0, gameTime * 360 / TotalGameTime);
-        if (gameTime > TotalGameTime)
+        lvlTime += Time.deltaTime; 
+        timeText.text =  "Level : "+  lvl.ToString("F2") + " health  " + healthTime.ToString("F2");
+        float rotationAmount = (healthStartTime - healthTime) * 360 / healthStartTime;
+        if (rotationAmount > 360)
         {
-            gameTime = 0;
+            rotationAmount = 360;
+        }
+        charachterBar360.transform.rotation = Quaternion.Euler(0, 0, rotationAmount);
+        if (healthTime < 0)
+        {
+            healthTime = 0;
             GameOver();
         }
+        if (lvlTime > lvlConstTime)
+        {
+            HandleNextLevelVariables( );
+        }
+    }
+    public void HandleNextLevelVariables( )
+    {
+        lvl += 1;
+        lvlTime = 0;
+        if (lvl>3)  return;
+        foodSpawnHandler.spawningTime -= 1;
+        
+
     }
 
     public void HandleFoodEaten()
-    {
-        gameTime -= 5f;
+    { 
+        healthTime +=  eatingFoodHealthAmount;
     }
 
     public void HandleFoodRotten()
     {
-        gameTime += 2f;
+        //healthTime -=  rottenFoodHealthAmount;
     }
 
     private void GameOver()
@@ -52,8 +81,8 @@ public class GameManager : MonoBehaviour
         // Additional game-over actions
     }
 
-    public float GetGameTime()
+    public float GethealthTime()
     {
-        return gameTime;
+        return healthTime;
     }
 }
